f54ab13ed0445c464c3f21082b2879f1fb7e3e8d
chethtrayen <chethtrayen1991@gmail.com>
2019-04-28 23:55:50 -0400

    Add sound to clicking logo

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,4 @@ function cycleCard() {
   const randomCard = cards.splice(randomCardIndex, 1)[0];
   cardContent.innerHTML = (cards.length > 0) ? `${randomCard.condition}, ${randomCard.cost}` : 'No more cards left!<hr>Contribute more here: <a href="https://github.com/tehjawn/weebflush">Github</a>'; 
 }
+
