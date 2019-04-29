6f9911031ed4ed5be6a3019c4fdba2d654cee54d
chethtrayen <chethtrayen1991@gmail.com>
2019-04-28 23:52:19 -0400

    Added swipe detection for mobile (#4)

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,29 @@
 
+const cardContent = document.getElementById('cardContent');
+const cardContainer = document.getElementsByClassName('card')[0];
+const swipeThreshold = 100;
+
 function cycleCard() {
-  const cardContent = document.getElementById('cardContent');
   const randomCardIndex = Math.floor(Math.random()*cards.length);
   const randomCard = cards.splice(randomCardIndex, 1)[0];
   cardContent.innerHTML = (cards.length > 0) ? `${randomCard.condition}, ${randomCard.cost}` : 'No more cards left!<hr>Contribute more here: <a href="https://github.com/tehjawn/weebflush">Github</a>'; 
 }
+
+function swipeDetection(){
+  let { originTouchX, endTouchX } = 0;
+  
+  cardContainer.addEventListener('touchstart', (ev)=>{
+    originTouchX = ev.changedTouches[0].screenX;
+  });
+
+  cardContainer.addEventListener('touchend', (ev)=>{
+    endTouchX = ev.changedTouches[0].screenX;
+    const touchResult = endTouchX - originTouchX;
+
+    if(touchResult > swipeThreshold){
+      cycleCard();
+    }
+  });
+}
+
+swipeDetection();
