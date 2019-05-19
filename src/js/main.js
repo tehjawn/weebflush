f02eeb53a65b010b28431dac20cbb2ac8369dcb9
John Nguyen <tehjawn@gmail.com>
2019-05-19 18:53:29 -0400

    3 cards at a time

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,35 @@
-
 let cards = CARDS;
 
 function cycleCard() {
+  const cardsToDraw = Math.min(3, cards.length);
+  const randomCardIndex = Math.floor(Math.random() * cards.length);
+
+  var cardsHtml = '';
+  if (cardsToDraw > 0) {
+    for (var i = 0; i < cardsToDraw; i++) {
+      const randomCardData = cards.splice(randomCardIndex, 1)[0];
+      cardsHtml += `
+        <div class="card-ctn">
+          <div class="card">
+            <span>
+              ${randomCardData.text}
+            </span>
+          </div>
+        </div>
+        `
+    }
+  } else {
+    cardsHtml = `
+    <div class="card-ctn">
+      <div class="card">
+        <span>
+          No more cards left!<hr>Contribute more here: <a href="https://github.com/tehjawn/weebflush">Github</a>
+        </span>
+      </div>
+    </div>
+    `;
+  }
   const cardContent = document.getElementById('cardContent');
-  const randomCardIndex = Math.floor(Math.random()*cards.length);
-  const randomCard = cards.splice(randomCardIndex, 1)[0];
-  // const randomCardText = `${randomCard.condition}, ${randomCard.cost}` ;
-  const randomCardText = `${randomCard.text}` ;
-
-  if (cards.length > 0){
-    responsiveVoice.speak(randomCardText);    
-    cardContent.innerHTML = randomCardText;
-  }
-  else{
-    playSound('finish game'); 
-    cardContent.innerHTML =  'No more cards left!<hr>Contribute more here: <a href="https://github.com/tehjawn/weebflush">Github</a>'; 
-  }
+  cardContent.innerHTML = cardsHtml;
+  cardContent.scrollLeft = 0;
 }
-
-
